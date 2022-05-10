cfb9e1dc8d1391c873c49edb829211df754cdcc1
Khalid Walit <harithkhalid93@gmail.com>
2022-05-10 10:49:42 +0800

    Create login with JWT

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals', require('./routers/goalRoutes'))
+app.use('/api/users', require('./routers/userRoutes'))
 
 app.use(errorHandler)
 
