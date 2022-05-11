d1321869e2e1dbfb6bc906786d8117f93e8412dd
Khalid Walit <harithkhalid93@gmail.com>
2022-05-11 13:43:19 +0800

    add delete goal function

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,9 +3,11 @@ import { useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import GoalForm from '../components/GoalForm'
 import GoalItem from '../components/GoalItem'
+import { getGoals, reset } from '../features/goals/goalSlice'
 
 function Dashboard() {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
   const { user } = useSelector((state) => state.auth)
   const { goals, isLoading, isError, message } = useSelector(
     (state) => state.goals
@@ -13,10 +15,21 @@ function Dashboard() {
 
   useEffect(() => {
 
+    if (isError) {
+      console.log(message)
+    }
+
     if (!user) {
       navigate('/login')
+    } else {
+      dispatch(getGoals())
+
+      return () => {
+        dispatch(reset())
+      }
     }
-  }, [user, navigate])
+
+  }, [user, navigate, isError, message, dispatch])
 
   return (
     <>
