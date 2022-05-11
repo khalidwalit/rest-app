43350921e70c600794a175f35aef47f680c45b49
Khalid Walit <harithkhalid93@gmail.com>
2022-05-11 18:42:53 +0800

    Resolve proxy issue

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://api.hvrange.xyz:8000',
+      changeOrigin: true,
+    })
+  );
+};
